8f1b73a0e2f03e50143e97a0ee763c3f07de1d16
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2023-10-20 11:55:50 +0300

    feat: add solution morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const letterSize = 10
+    const binarySize = 2
+    const resultArrNum = []
+    const resultArrMorse = []
+    let resultText = ''
+    let morseLetter = ''
+    const dot = 10
+    const dashes = 11
+    const space = '**********'
+
+
+    for (let i = 0; i < expr.length; i+= letterSize) {
+        resultArrNum.push(expr.slice(i, i + letterSize))
+    }
+    resultArrNum.map(el => {
+        if (el == space) {
+            resultArrMorse.push(' ')
+        } else {
+            for (let j = 0; j < el.length; j+= binarySize) {
+                let pair = el.slice(j, j + binarySize)
+                if(pair == dot){
+                    morseLetter += '.'
+                } else if (pair == dashes) {
+                    morseLetter += '-'
+                }
+            }
+            resultArrMorse.push(morseLetter)
+            morseLetter = ''
+        }
+    })
+    resultArrMorse.map( el => {
+        if (el == ' '){
+            resultText += ' '
+        }
+        for (const morsetableKey in MORSE_TABLE) {
+            if (el == morsetableKey) {
+                let letter = MORSE_TABLE[morsetableKey]
+                resultText += letter
+            }
+        }
+    })
+    return resultText
 }
 
 module.exports = {
